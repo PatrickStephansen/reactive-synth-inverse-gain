25be317e1c57e9237fd2412821321669cb75c8a1
Patrick Stephansen <pstephansen89@gmail.com>
2020-07-04 18:51:18 +0200

    send a message when the module is ready to start processing

--- worklet/inverse-gain.js
+++ worklet/inverse-gain.js
@@ -3,35 +3,37 @@ const defaultMax = 1e9;
 const bytesPerSample = 4;
 
 registerProcessor(
-  'reactive-synth-inverse-gain',
+  "reactive-synth-inverse-gain",
   class InverseGain extends AudioWorkletProcessor {
     static get parameterDescriptors() {
       return [
         {
-          name: 'divisor',
+          name: "divisor",
           defaultValue: 10,
-          automationRate: 'a-rate'
+          automationRate: "a-rate",
         },
         {
-          name: 'zeroDivisorFallback',
+          name: "zeroDivisorFallback",
           defaultValue: 0,
-          automationRate: 'a-rate'
-        }
+          automationRate: "a-rate",
+        },
       ];
     }
 
     constructor() {
       super();
-      this.port.onmessage = event => {
-        if (event.data.type === 'wasm') {
-          this.initWasmModule(event.data.wasmModule);
+      this.port.onmessage = (event) => {
+        if (event.data.type === "wasm") {
+          this.initWasmModule(event.data.wasmModule).then(() =>
+            this.port.postMessage({ type: "module-ready", value: true })
+          );
         }
       };
     }
 
     async initWasmModule(wasmModule) {
       this.wasmModule = await WebAssembly.instantiate(wasmModule, {
-        console: { log: s => console.log('message from wasm', s) }
+        console: { log: (s) => console.log("message from wasm", s) },
       });
       this.internalProcessorPtr = this.wasmModule.exports.init(
         defaultMin,
@@ -41,7 +43,9 @@ registerProcessor(
         defaultMin,
         defaultMax
       );
-      this.float32WasmMemory = new Float32Array(this.wasmModule.exports.memory.buffer);
+      this.float32WasmMemory = new Float32Array(
+        this.wasmModule.exports.memory.buffer
+      );
     }
 
     process(inputs, outputs, parameters) {
@@ -50,16 +54,19 @@ registerProcessor(
       if (inputs[0][0].length && this.wasmModule) {
         this.float32WasmMemory.set(
           inputs[0][0],
-          this.wasmModule.exports.get_quotient_ptr(this.internalProcessorPtr) / bytesPerSample
+          this.wasmModule.exports.get_quotient_ptr(this.internalProcessorPtr) /
+            bytesPerSample
         );
         this.float32WasmMemory.set(
           parameters.divisor,
-          this.wasmModule.exports.get_divisor_ptr(this.internalProcessorPtr) / bytesPerSample
+          this.wasmModule.exports.get_divisor_ptr(this.internalProcessorPtr) /
+            bytesPerSample
         );
         this.float32WasmMemory.set(
           parameters.zeroDivisorFallback,
-          this.wasmModule.exports.get_divisor_fallback_ptr(this.internalProcessorPtr) /
-            bytesPerSample
+          this.wasmModule.exports.get_divisor_fallback_ptr(
+            this.internalProcessorPtr
+          ) / bytesPerSample
         );
         const outputPointer =
           this.wasmModule.exports.process_quantum(
@@ -68,10 +75,20 @@ registerProcessor(
             parameters.divisor.length,
             parameters.zeroDivisorFallback.length
           ) / bytesPerSample;
-        for (let channelIndex = 0; channelIndex < outputs[0].length; channelIndex++) {
+        for (
+          let channelIndex = 0;
+          channelIndex < outputs[0].length;
+          channelIndex++
+        ) {
           // TODO: can this not be done with some array util that's faster?
-          for (let sample = 0; sample < outputs[0][channelIndex].length; sample++) {
-            outputs[0][channelIndex][sample] = this.float32WasmMemory[outputPointer + sample];
+          for (
+            let sample = 0;
+            sample < outputs[0][channelIndex].length;
+            sample++
+          ) {
+            outputs[0][channelIndex][sample] = this.float32WasmMemory[
+              outputPointer + sample
+            ];
           }
         }
       }
